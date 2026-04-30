81694708d49d5d7427372c0fef9bd4a1c73a8915
cf-webmaster <webmaster@cfbc.org>
2026-04-30 14:56:20 -0500

    Add YTH scrapbook prototype page

--- includes/header.php
+++ includes/header.php
@@ -121,6 +121,7 @@ $base_url = $base_url ?? '/';
         <ul>
           <li><a href="index.php"<?php if (($current_page ?? '') === 'index') echo ' class="active"'; ?>>Home</a></li>
           <li><a href="index.php#visit">Visit</a></li>
+          <li><a href="youthgroup/"<?php if (($current_page ?? '') === 'youthgroup') echo ' class="active"'; ?>>YTH</a></li>
           <li><a href="events/"<?php if (($current_page ?? '') === 'events') echo ' class="active"'; ?>>Events</a></li>
           <li><a href="index.php#rhythms">Life at Declaration</a></li>
           <li><a href="index.php#next-steps">Next Steps</a></li>
