fa9e03ccc0b722e80bd97683feb79748d2b11c48
cf-webmaster <webmaster@cfbc.org>
2026-09-03 17:44:18 -0500

    Add Declaration favicon to CMS pages

--- cms/_header.php
+++ cms/_header.php
@@ -10,6 +10,8 @@ $cmsCurrent = $cmsCurrent ?? 'events';
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="robots" content="noindex, nofollow">
   <title><?= cms_escape($cmsPageTitle) ?> | Declaration CMS</title>
+  <link rel="icon" type="image/png" href="/assets/img/declaration/Declaration-Church_website.png">
+  <link rel="apple-touch-icon" href="/assets/img/declaration/Declaration-Church_website.png">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@500;600;700;800&family=Inter+Tight:wght@400;500;600&display=swap" rel="stylesheet">
